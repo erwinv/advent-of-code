bf14c375d56567cd3760916ca023b435a39d0973
Erwin Villejo <erwin.villejo@gmail.com>
2021-12-15 21:51:48 +0700

    refactor 14

--- 2021/14.ts
+++ 2021/14.ts
@@ -39,123 +39,63 @@ export function parseInput(s: string): Input {
 
 type ElementCounters = Record<Element, number>
 
-const polymerize10 = _.memoize((ancestorPair: Pair, rules: Input['insertionRules']) => {
-  function step(template: Polymer, stepsRemaining: number): Polymer {
-    if (stepsRemaining === 0) return template
+function polymerize(template: Polymer, rules: Input['insertionRules'], steps: number): ElementCounters {
+  const pairChildren = {} as Record<Pair, {
+    child: Element
+    pairs: [Pair, Pair]
+  }>
 
-    const polymer = [...template]
-    let i = 0
-    let insertedOffset = 0
-    for (const [e1, e2] of slidingPairs(template)) {
-      const child = rules.get(`${e1}${e2}`)
-      if (child) {
-        polymer.splice(i + insertedOffset + 1, 0, child)
-        insertedOffset++
-      }
-      i++
+  for (const [pair, child] of rules) {
+    const pairArr = pair.split('')
+    pairArr.splice(1, 0, child)
+    pairChildren[pair] = {
+      child: child,
+      pairs: [pairArr.slice(0, 2).join(''), pairArr.slice(1).join('')]
     }
-
-    return step(polymer, stepsRemaining - 1)
   }
 
-  const polymerAfter10 = step(ancestorPair.split('') as Polymer, 10)
+  const elementCounts = _.countBy(template, _.identity) as Record<Element, number>
+  const pairCounts = {} as Record<Pair, number>
 
-  const polymerChildrenAfter10 = polymerAfter10.slice(1, -1)
+  for (const [e1, e2] of slidingPairs(template)) {
+    const pair = `${e1}${e2}`
+    pairCounts[pair] = (pairCounts[pair] ?? 0) + 1
+  }
 
-  return [
-    polymerAfter10,
-    _.countBy(polymerChildrenAfter10, _.identity) as ElementCounters,
-  ] as const
-})
+  for (const __ of _.range(steps)) {
+    const childPairCounts = {} as Record<Pair, number>
+
+    for (const [pair, count] of Object.entries(pairCounts)) {
+      const {
+        child,
+        pairs: [childPair1, childPair2],
+      } = pairChildren[pair]
+
+      elementCounts[child] = (elementCounts[child] ?? 0) + count
+      childPairCounts[childPair1] = (childPairCounts[childPair1] ?? 0) + count
+      childPairCounts[childPair2] = (childPairCounts[childPair2] ?? 0) + count
+
+      pairCounts[pair] = pairCounts[pair] - count
+    }
+
+    for (const [pair, count] of Object.entries(childPairCounts)) {
+      pairCounts[pair] = (pairCounts[pair] ?? 0) + count
+    }
+  }
+
+  return elementCounts
+}
 
 export function part1({template, insertionRules}: Input) {
-  const polymerCounters = _.countBy(template, _.identity) as ElementCounters
-
-  for (const pair of slidingPairs(template)) {
-    const [, counters] = polymerize10(pair.join('') as Pair, insertionRules)
-    for (const [el, counter] of Object.entries(counters)) {
-      polymerCounters[el as Element] = (polymerCounters[el as Element] ?? 0) + counter
-    }
-  }
-
-  const elementCountsAsc = Object.values(polymerCounters)
+  const elementCounts = polymerize(template, insertionRules, 10)
+  const elementCountsAsc = Object.values(elementCounts)
     .sort(_.subtract)
   return _.last(elementCountsAsc)! - _.first(elementCountsAsc)!
 }
 
 export function part2({template, insertionRules}: Input) {
-  const elements: Set<Element> = new Set()
-  for (const element of template) {
-    elements.add(element)
-  }
-  for (const [pair, child] of insertionRules.entries()) {
-    elements.add(pair.at(0) as Element)
-    elements.add(pair.at(1) as Element)
-    elements.add(child)
-  }
-  const allPairs = Array.from(pairPermutations(Array.from(elements)))
-
-  const childrenCountersAfter20 = {} as Record<Pair, ElementCounters>
-  for (const ancestorPair of allPairs) {
-    const ancestor = ancestorPair.join('') as Pair
-    const [polymerAfter10] = polymerize10(ancestor, insertionRules)
-
-    childrenCountersAfter20[ancestor] = _.countBy(polymerAfter10.slice(1, -1), _.identity) as ElementCounters
-    
-    for (const pair_ of slidingPairs(polymerAfter10)) {
-      const [, counters] = polymerize10(pair_.join('') as Pair, insertionRules)
-      
-      for (const [el, counter] of Object.entries(counters)) {
-        childrenCountersAfter20[ancestor][el as Element] =
-        (childrenCountersAfter20[ancestor][el as Element] ?? 0) + counter
-      }
-    }
-  }
-
-  const childrenCountersAfter30 = {} as Record<Pair, ElementCounters>
-  for (const ancestorPair of allPairs) {
-    const ancestor = ancestorPair.join('') as Pair
-    const [polymerAfter10] = polymerize10(ancestor, insertionRules)
-
-    childrenCountersAfter30[ancestor] = _.countBy(polymerAfter10.slice(1, -1), _.identity) as ElementCounters
-
-    for (const pair_ of slidingPairs(polymerAfter10)) {
-      const counters = childrenCountersAfter20[pair_.join('') as Pair]
-
-      for (const [el, counter] of Object.entries(counters)) {
-        childrenCountersAfter30[ancestor][el as Element] =
-          (childrenCountersAfter30[ancestor][el as Element] ?? 0) + counter
-      }
-    }
-  }
-
-  const childrenCountersAfter40 = {} as Record<Pair, ElementCounters>
-  for (const ancestorPair of allPairs) {
-    const ancestor = ancestorPair.join('') as Pair
-    const [polymerAfter10] = polymerize10(ancestor, insertionRules)
-
-    childrenCountersAfter40[ancestor] = _.countBy(polymerAfter10.slice(1, -1), _.identity) as ElementCounters
-
-    for (const pair_ of slidingPairs(polymerAfter10)) {
-      const counters = childrenCountersAfter30[pair_.join('') as Pair]
-
-      for (const [el, counter] of Object.entries(counters)) {
-        childrenCountersAfter40[ancestor][el as Element] =
-          (childrenCountersAfter40[ancestor][el as Element] ?? 0) + counter
-      }
-    }
-  }
-
-  const polymerCounters = _.countBy(template, _.identity) as ElementCounters
-  for (const pair of slidingPairs(template)) {
-    const counters = childrenCountersAfter40[pair.join('') as Pair]
-
-    for (const [el, counter] of Object.entries(counters)) {
-      polymerCounters[el as Element] = (polymerCounters[el as Element] ?? 0) + counter
-    }
-  }
-
-  const elementCountsAsc = Object.values(polymerCounters)
+  const elementCounts = polymerize(template, insertionRules, 40)
+  const elementCountsAsc = Object.values(elementCounts)
     .sort(_.subtract)
   return _.last(elementCountsAsc)! - _.first(elementCountsAsc)!
 }
